6b614b6dca90e64eef5e5a547612b462242c7dcb
Jnnsm <eduardo.j.moreira@ufv.br>
2018-11-15 09:12:03 -0200

    Adicionado a detecção de caixa, rolagem pelo roda do mouse, mudança da detecção de "typing"

--- Funcoes.h
+++ Funcoes.h
@@ -11,22 +11,33 @@
 *	Alterar nome das variáveis
 */
 
-
 extern vector<Objeto> objs;
 extern double __WIDTH, __HEIGHT;
 
+/* Armazenam respectivamente a quantidade de poligonos na tela e o tempo para desenha-los */
 int poly = 0,
 	deltaTime = 0;
 
+/* Representa o scroll do menu */
 int menuPos = 0;
+
+/* Variável teste para rotação */
 int rotatex = 0, rotatey = 0;
 
+/* Aspect Rato da tela e posição do observador */
 double aspectRatio = 1.0f * __WIDTH / __HEIGHT;
 GLdouble viewer[] = { 3.0, 3.0, 6.0 };
 
+/* String digitada na caixa de importação do objeto */
 string nameBox = "";
 
-bool typing = false;
+/* Identificadores para saber se o usuário está digitando						*/
+/* clickedObj = -1 representa a tela de menu inteira							*/
+/* clickedObj >= 0 representa um objeto de "objs"								*/
+/* typingField = -1 representa lugar nenhum										*/
+/* typingField = 0 representa a caixa de importação caso clickedObj = -1		*/
+/* typingField = 0 e <= 10 representam caixas do objeto quanto clickedObj != -1 */
+short clickedObj = -1, typingField = -1;
 
 /* Função para carregar objeto */
 void createObj(string fileName, Quad<double> color) {
@@ -47,10 +58,12 @@ void createObj(string fileName, Quad<double> color) {
 /* i - k = comandos que movem o "menu"			 */
 /* w - a - s - d = comandos que movem os objetos */
 
-void keyboard(unsigned char key, int x, int y) {	
-	if (typing) {
+void keyboard(unsigned char key, int x, int y) {
+	cout << x << " " << y << endl;
+	if (clickedObj == -1 && typingField == 0) {
 		if (key == 13) {
-			typing = false;
+			clickedObj = -1;
+			typingField = -1;
 			Quad<double> aux((double)rand() / (RAND_MAX), (double)rand() / (RAND_MAX), (double)rand() / (RAND_MAX), 0.5);
 			createObj(nameBox, aux);
 			nameBox = "";
@@ -91,29 +104,38 @@ void keyboard(unsigned char key, int x, int y) {
 
 /* Defne os cliques do mouse */
 void mouse(int button, int state, int x, int y) {
-	if (x >= 3 * __WIDTH / 4) {
-		
-		if (button == GLUT_LEFT_BUTTON) {
-			
-			if (state == GLUT_DOWN) {
-				double xf, yf;
-				xf = (200 * aspectRatio) * (x - 3 * __WIDTH / 4) / __WIDTH;
-				yf = (200) * (__HEIGHT - y) / __HEIGHT;
+	/* Trata rolagem do mouse */
+	if (button == 4)
+		menuPos++;
+	else if (button == 3)
+		menuPos--;
+	/* Trata parte do menu da tela */
+	else if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN){
+		if (x >= 3 * __WIDTH / 4) {
+			double xf, yf;
+			xf = (200 * aspectRatio) * (x - 3 * __WIDTH / 4) / __WIDTH;
+			yf = (200) * (__HEIGHT - y) / __HEIGHT;
 
-				/* Checa se ele está clicando na caixa de importação */
-				if (xf >= 20.0f / 3 && yf >= 180 && xf <= 60 && yf <= 188) {
-					typing = true;
-				}
-				else if (typing){
-					typing = false;
-				}
+			/* Descobre qual caixa o usuário clicou */
+			cout << trunc(((yf - 140 + menuPos) / -40)+0.99) << endl;
 
+			/* Checa se ele está clicando na caixa de importação */
+			if (xf >= 20.0f / 3 && yf >= 180 && xf <= 60 && yf <= 188) {
+				clickedObj = -1;
+				typingField = 0;
+			}
+			else if (clickedObj >= 0 || typingField >= 0) {
+				clickedObj = -1;
+				typingField = -1;
 			}
 		}
 	}
+	/* Trata a parte dos objetos da tela */
 	else {
 
 	}
+
+
 
 }
 
@@ -241,6 +263,94 @@ void displayMenu() {
 	glColor4f(0.88, 0.88, 0.88, 0.60);
 	glRectd(0.0, 0.0, 200 * aspectRatio / 4, 200.0);
 
+	/* Damos um push na matrix para não precisarmos redefinir o lookAt novamente para posição original */
+	
+	glPushMatrix();
+
+		gluLookAt(
+			0, menuPos, 1,
+			0.0, menuPos, 0.0,
+			0.0, 1.0, 0.0
+		);
+
+		/* Desenha cada quadrado para exemplo da caixa de cada objeto */
+		int pos = 0;
+		for (int i = 0; i < objs.size(); i++) {
+			pos = i * 40;
+
+			/* Desenha apenas se ele estiver a baixo da barra que importa objetos */
+			if (180 - pos - 40 - menuPos <= 180) {
+				/* Desenha caixa de fundo com a cor do objeto */
+				glColor4f(objs.at(i).rgba.primeiro, objs.at(i).rgba.segundo, objs.at(i).rgba.terceiro, 0.7);
+				glRectd(20.0f / 3, 180 - pos - 40, 60, 180 - (pos + 5));
+
+				glColor4f(0, 0, 0, 1);
+
+				/* Desenha o nome do objeto */
+				glPushMatrix();
+
+				glTranslated(23.0f / 3, 175 - pos - 5, 0);
+				glScaled(0.035, 0.035, 0);
+				glutStrokeString(GLUT_STROKE_ROMAN, (unsigned char*)(objs.at(i).name).c_str());
+
+				glPopMatrix();
+
+				/* Desenha as caixas relacionadas com translação e seu label */
+				glPushMatrix();
+
+				glTranslated(23.0f / 3, 168 - (pos + 5), 0);
+				glScaled(0.035, 0.035, 0);
+				glutStrokeString(GLUT_STROKE_ROMAN, (unsigned char*)"T: ");
+
+				glPopMatrix();
+
+
+				glRectd(24, 168 - pos, 34, 168 - (pos + 5));
+				glRectd(36, 168 - pos, 46, 168 - (pos + 5));
+				glRectd(48, 168 - pos, 58, 168 - (pos + 5));
+
+				/* Desenha as caixas relacionadas com rotação e seu label */
+				glPushMatrix();
+
+				glTranslated(23.0f / 3, 161 - (pos + 5), 0);
+				glScaled(0.035, 0.035, 0);
+				glutStrokeString(GLUT_STROKE_ROMAN, (unsigned char*)"R: ");
+
+				glPopMatrix();
+
+				glRectd(12, 161 - (pos), 22, 161 - (pos + 5));
+				glRectd(24, 161 - (pos), 34, 161 - (pos + 5));
+				glRectd(36, 161 - (pos), 46, 161 - (pos + 5));
+				glRectd(48, 161 - (pos), 58, 161 - (pos + 5));
+
+				/* Desenha as caixas relacionadas com escala e seu label */
+				glPushMatrix();
+
+				glTranslated(23.0f / 3, 154 - (pos + 5), 0);
+				glScaled(0.035, 0.035, 0);
+				glutStrokeString(GLUT_STROKE_ROMAN, (unsigned char*)"E: ");
+
+				glPopMatrix();
+
+				glRectd(24, 154 - pos, 34, 154 - (pos + 5));
+				glRectd(36, 154 - pos, 46, 154 - (pos + 5));
+				glRectd(48, 154 - pos, 58, 154 - (pos + 5));
+
+				/* Desenha as caixas relacionadas com transparencia e seu label */
+				glPushMatrix();
+
+				glTranslated(23.0f / 3, 147 - (pos + 5), 0);
+				glScaled(0.035, 0.035, 0);
+				glutStrokeString(GLUT_STROKE_ROMAN, (unsigned char*)"Alpha: ");
+
+				glPopMatrix();
+
+				glRectd(48, 147 - pos, 58, 147 - (pos + 5));
+			}
+		}
+
+	glPopMatrix();
+
 	/* Caixa para digitar arquivo */
 	glPushMatrix();
 		glColor4d(0, 0, 0, 1);
@@ -257,92 +367,6 @@ void displayMenu() {
 		glTranslated(23.0f / 3, 182, 0);
 		glScaled(0.035, 0.035, 0);
 		glutStrokeString(GLUT_STROKE_ROMAN, (unsigned char*)(nameBox + "_").c_str());
-	glPopMatrix();
-
-
-	/* Damos um push na matrix para não precisarmos redefinir o lookAt novamente para posição original */
-	
-	glPushMatrix();
-
-		gluLookAt(
-			0, menuPos, 1,
-			0.0, menuPos, 0.0,
-			0.0, 1.0, 0.0
-		);
-
-		/* Desenha cada quadrado para exemplo da caixa de cada objeto */
-		int pos = 0;
-		for (int i = 0; i < objs.size(); i++) {
-			pos = i * 40;
-			/* Desenha caixa de fundo com a cor do objeto */
-			glColor4f(objs.at(i).rgba.primeiro, objs.at(i).rgba.segundo, objs.at(i).rgba.terceiro, 0.7);
-			glRectd(20.0f/3, 180 - pos - 40, 60, 180 - (pos + 5));
-
-			glColor4f(0, 0, 0, 1);
-
-			/* Desenha o nome do objeto */
-			glPushMatrix();
-
-				glTranslated(23.0f / 3, 175 - pos - 5, 0);
-				glScaled(0.035, 0.035, 0);
-				glutStrokeString(GLUT_STROKE_ROMAN, (unsigned char*)(objs.at(i).name).c_str());
-
-			glPopMatrix();
-
-			/* Desenha as caixas relacionadas com translação e seu label */
-			glPushMatrix();
-
-				glTranslated(23.0f / 3, 168 - (pos + 5), 0);
-				glScaled(0.035, 0.035, 0);
-				glutStrokeString(GLUT_STROKE_ROMAN, (unsigned char*)"T: ");
-
-			glPopMatrix();
-
-			
-			glRectd(24, 168 - pos, 34, 168 - (pos + 5));
-			glRectd(36, 168 - pos, 46, 168 - (pos + 5));
-			glRectd(48, 168 - pos, 58, 168 - (pos + 5));
-
-			/* Desenha as caixas relacionadas com rotação e seu label */
-			glPushMatrix();
-
-				glTranslated(23.0f / 3, 161 - (pos + 5), 0);
-				glScaled(0.035, 0.035, 0);
-				glutStrokeString(GLUT_STROKE_ROMAN, (unsigned char*)"R: ");
-
-			glPopMatrix();
-
-			glRectd(12, 161 - (pos), 22, 161 - (pos + 5));
-			glRectd(24, 161 - (pos), 34, 161 - (pos + 5));
-			glRectd(36, 161 - (pos), 46, 161 - (pos + 5));
-			glRectd(48, 161 - (pos), 58, 161 - (pos + 5));
-
-			/* Desenha as caixas relacionadas com escala e seu label */
-			glPushMatrix();
-
-				glTranslated(23.0f / 3, 154 - (pos + 5), 0);
-				glScaled(0.035, 0.035, 0);
-				glutStrokeString(GLUT_STROKE_ROMAN, (unsigned char*)"E: ");
-
-			glPopMatrix();
-
-			glRectd(24, 154 - pos, 34, 154 - (pos + 5));
-			glRectd(36, 154 - pos, 46, 154 - (pos + 5));
-			glRectd(48, 154 - pos, 58, 154 - (pos + 5));
-
-			/* Desenha as caixas relacionadas com transparencia e seu label */
-			glPushMatrix();
-
-				glTranslated(23.0f / 3, 147 - (pos + 5), 0);
-				glScaled(0.035, 0.035, 0);
-				glutStrokeString(GLUT_STROKE_ROMAN, (unsigned char*)"Alpha: ");
-
-			glPopMatrix();
-
-			glRectd(48, 147 - pos, 58, 147 - (pos + 5));
-
-		}
-
 	glPopMatrix();
 
 	/* Escreve a quantidade de tempo para desenhar e a quantidade de poly na parte de baixo da tela */
